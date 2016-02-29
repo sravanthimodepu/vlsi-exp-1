f6e72c521de136a819d2f6e6a6cf693e50c24318
Sravanthimodepu <sravanthimodepu@gmail.com>
2016-02-29 17:54:35 +0530

    added src files of vlsi lab

---
+++ new-vlsi/index.js
@@ -0,0 +1,11 @@
+ function setImg(a){
+      // alert(a.getElementsByTagName('img')[0].src);
+    document.getElementById('ImageFrame').src = a.getElementsByTagName('img')[0].src
+    document.getElementById('ImageFrame').src = a.getElementsByTagName('rect')[0].src
+    
+
+            }
+
+            function myfunction() {
+            	alert("For this Exp yout don't need this component!");
+            }
---
+++ new-vlsi/jquery.js
@@ -0,0 +1,12 @@
+
+function myfunction() {
+$(document).ready (function() {
+	$ ("#comp1").click(function()) {
+		$("comp0").hide();
+	};
+	$ ("#comp0").click(function()) {
+		$("comp1").show();
+	};
+	}
+}
+// "$('#comp0').hide(); $('#comp1').show()"
